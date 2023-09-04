5321f4af8cf547b1bbcb6257719c09055c54de78
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-09-04 16:34:50 +0700

    - Added folder 20230904 - Completing DivisibleSumPairs, and SparseArrays

---
+++ Competitive Programming/Solved/20230904/DivisibleSumPairs.cs
@@ -0,0 +1,69 @@
+﻿namespace DivisibleSumPairs
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'divisibleSumPairs' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts following parameters:
+         *  1. INTEGER n
+         *  2. INTEGER k
+         *  3. INTEGER_ARRAY ar
+         *  
+         *  To make it simple, this function supposed to search pair of number within ar, where if they are sum, it must be divisible by k.
+         *  The math operation must be done from index 0 to n. So there must no any ar[5] + ar[4] stuffs. If 5 is i and 4 is j, then I can say, the i must be lesser than j.
+         *  The output is the count of pairs that available.
+         */
+
+        public static int divisibleSumPairs(int n, int k, List<int> ar)
+        {
+            // Flag count of pairs that available.
+            int pairs = 0;
+
+            // Loop every value within ar
+            for (int i = 0; i < ar.Count; i++)
+            {
+                // Starting from the next of current selected value to be sum with
+                for (int j = i + 1; j < ar.Count; j++)
+                {
+                    // If ar[i] sum with ar[j] is divisible with k, then add 1 to pairs var.
+                    if ((ar[i] + ar[j]) % k == 0)
+                    {
+                        pairs++;
+                    }
+                }
+            }
+
+            return pairs;
+        }
+
+    }
+
+    class Solution
+    {
+        public static void DivisibleSumPairs(string[] args)
+        {
+            // Comment this when running in your local
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int k = Convert.ToInt32(firstMultipleInput[1]);
+
+            List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+            int result = Result.divisibleSumPairs(n, k, ar);
+
+            // Comment this when running in your local
+            textWriter.WriteLine(result);
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+
+}
+
---
+++ Competitive Programming/Solved/20230904/SparseArrays.cs
@@ -0,0 +1,79 @@
+﻿namespace SparseArrays
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'matchingStrings' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts following parameters:
+         *  1. STRING_ARRAY strings
+         *  2. STRING_ARRAY queries
+         *  
+         *  To simplify, this problem is searching text within strings by quering within queries in precise technique. (Not using %LIKE%).
+         *  The expected return is the list of count number that came from queries.
+         *  @link https://www.hackerrank.com/challenges/three-month-preparation-kit-sparse-arrays/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-one
+         */
+
+        public static List<int> matchingStrings(List<string> strings, List<string> queries)
+        {
+            List<int> foundCount = new List<int>();
+
+            for (int i = 0; i < queries.Count; i++)
+            {
+                int currentQueryFoundCount = 0;
+                string e = queries[i];
+
+                foreach (string str in strings)
+                {
+                    if (str.Equals(e))
+                    {
+                        currentQueryFoundCount++;
+                    }
+                }
+
+                foundCount.Add(currentQueryFoundCount);
+            }
+
+            return foundCount;
+        }
+
+    }
+
+    class Solution
+    {
+        public static void sparseArray(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int stringsCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<string> strings = new List<string>();
+
+            for (int i = 0; i < stringsCount; i++)
+            {
+                string stringsItem = Console.ReadLine();
+                strings.Add(stringsItem);
+            }
+
+            int queriesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<string> queries = new List<string>();
+
+            for (int i = 0; i < queriesCount; i++)
+            {
+                string queriesItem = Console.ReadLine();
+                queries.Add(queriesItem);
+            }
+
+            List<int> res = Result.matchingStrings(strings, queries);
+
+            textWriter.WriteLine(String.Join("\n", res));
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+
+}
