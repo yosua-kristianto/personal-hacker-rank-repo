56129ed67e40086f688b72eaf904dab7ef3cff7c
yosuakristianto <yosua_kristianto144@outlook.com>
2023-09-06 17:45:33 +0700

    Completing Grading and Lonely Integer

---
+++ Competitive Programming/Solved/20230906/Grading.cs
@@ -0,0 +1,70 @@
+﻿namespace Grading
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'gradingStudents' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts INTEGER_ARRAY grades as parameter.
+         * 
+         * So basically, this problem is to round up students grade whatever the grade is ((closest multiple by 5 number) - grade) < 3.
+         * But this rule is only apply if the score is above 38 since 40 is a consider as failing.
+         * 
+         * Example:
+         * if grade is 84, then you should round it up to 85 since (85 - 84) < 3
+         * If grade is 29, then you should not round it up since the number is below 38.
+         * If grade is 57, then you should not round it up to 60 since (60 - 57) = 3
+         */
+
+        public static List<int> gradingStudents(List<int> grades)
+        {
+            for (int i = 0; i < grades.Count; i++)
+            {
+                // If grade is less than 38, skip this loop
+                if (grades[i] < 38) { continue; }
+
+                // Make a 5 multiplication started from f(8) = 40 to f(20) = 100
+                for (int j = 8; j < 21; j++)
+                {
+                    // Find the most closest positive integer to zero for the e, and round up the value.
+                    if (((5 * j) - grades[i]) < 3 && ((5 * j) - grades[i]) > 0)
+                    {
+                        grades[i] = (5 * j);
+                    }
+                }
+            }
+
+            return grades;
+        }
+
+    }
+
+    class Solution
+    {
+        public static void Gradings(string[] args)
+        {
+            // Comment this to run in local
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int gradesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> grades = new List<int>();
+
+            for (int i = 0; i < gradesCount; i++)
+            {
+                int gradesItem = Convert.ToInt32(Console.ReadLine().Trim());
+                grades.Add(gradesItem);
+            }
+
+            List<int> result = Result.gradingStudents(grades);
+
+            // Comment this to run in local
+            textWriter.WriteLine(String.Join("\n", result));
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+
+}
---
+++ Competitive Programming/Solved/20230906/LonelyInteger.cs
@@ -0,0 +1,72 @@
+﻿namespace LonelyInteger
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'lonelyinteger' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts INTEGER_ARRAY a as parameter.
+         * @link https://www.hackerrank.com/challenges/three-month-preparation-kit-lonely-integer/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two
+         */
+
+        public static int lonelyinteger(List<int> a)
+        {
+            // This is a data structure to store whatever non-unique value found.
+            List<int> nonUniqueFound = new List<int>();
+
+            for (int i = 0; i < (a.Count); i++)
+            {
+                bool foundFlag = false;
+
+                // If current loop has a number that already stored in nonUniqueFound, skip this loop.
+                if (nonUniqueFound.Contains(a[i]))
+                {
+                    continue;
+                }
+
+                // Loop through series starting from i+1
+                for (int j = i + 1; j < a.Count; j++)
+                {
+                    // If the series is same as a[i], then set the foundFlag to true, and store the value to nonUniqueValue
+                    if (a[i] == a[j])
+                    {
+                        foundFlag = true;
+                        nonUniqueFound.Add(a[i]);
+                        break;
+                    }
+                }
+
+                if (!foundFlag)
+                {
+                    Console.WriteLine(a[i]);
+                    return a[i];
+                }
+            }
+
+            throw new Exception("There are no unique number within the array series.");
+        }
+    }
+
+    class Solution
+    {
+        public static void LonelyIntegers(string[] args)
+        {
+            // Comment this to run in local
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            int result = Result.lonelyinteger(a);
+
+            // Comment this to run in local
+            textWriter.WriteLine(result);
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+
+}
