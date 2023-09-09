3d6238df9512e2d9e487f3aadee063f8375fdf58
yosuakristianto <yosua_kristianto144@outlook.com>
2023-09-09 15:55:34 +0700

    Added MarsExploration and Pangram

---
+++ Competitive Programming/Solved/20230909/Mars Exploration.cs
@@ -0,0 +1,62 @@
+﻿namespace MarsExploration
+{
+    using System.CodeDom.Compiler;
+    using System.Collections.Generic;
+    using System.Collections;
+    using System.ComponentModel;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Runtime.Serialization;
+    using System.Text.RegularExpressions;
+    using System.Text;
+    using System;
+
+    class Result
+    {
+
+        /*
+         * Complete the 'marsExploration' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts STRING s as parameter.
+         */
+
+        public static int marsExploration(string s)
+        {
+            int differenceFlag = 0;
+
+            for (int i = 0; i < s.Length; i += 3)
+            {
+                string processString = s.ToUpper();
+
+                if (processString[i] != 'S') differenceFlag++;
+                if (processString[i + 1] != 'O') differenceFlag++;
+                if (processString[i + 2] != 'S') differenceFlag++;
+            }
+
+            return differenceFlag;
+        }
+
+    }
+
+    class Solution
+    {
+        public static void MarsExplorations(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string s = Console.ReadLine();
+
+            int result = Result.marsExploration(s);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+
+}
---
+++ Competitive Programming/Solved/20230909/Pangram.cs
@@ -0,0 +1,46 @@
+﻿namespace Pangram
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'pangrams' function below.
+         *
+         * The function is expected to return a STRING.
+         * The function accepts STRING s as parameter.
+         */
+
+        public static string pangrams(string s)
+        {
+            // Whatever any letter not found in from the text, return not pangram.
+            foreach (char e in "abcdefghijklmnopqrstuvwyxz")
+            {
+                if (!s.ToLower().Contains(e))
+                {
+                    return "not pangram";
+                }
+            }
+
+            return "pangram";
+        }
+
+    }
+
+    class Solution
+    {
+        public static void Pangrams(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string s = Console.ReadLine();
+
+            string result = Result.pangrams(s);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+
+}
