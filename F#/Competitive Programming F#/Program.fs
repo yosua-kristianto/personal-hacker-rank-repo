a1a9a5cc7abcc61ff7735c43f3a83f51d08b3a91
yosuakristianto <yosua_kristianto144@outlook.com>
2023-09-28 19:53:17 +0700

    Initial F#

--- F#/Competitive Programming F#/Program.fs
+++ F#/Competitive Programming F#/Program.fs
@@ -1,2 +1,13 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿(*
+    @link https://www.hackerrank.com/challenges/three-month-preparation-kit-two-arrays/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-three
+*)
+open System
+
+// Input q
+let Q: Int32 = Int32.Parse(Console.ReadLine());
+
+// Input n and k
+let KAndN: String[] = Console.ReadLine().Split(' ');
+
+let K: Int32 = Int32.Parse(KAndN.[1]);
+let N: Int32 = Int32.Parse(KAndN.[0]);
